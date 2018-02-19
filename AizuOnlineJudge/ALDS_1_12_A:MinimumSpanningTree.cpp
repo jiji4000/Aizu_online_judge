36d8aa498b822bdfb276eacb802f5b5ecfa6c87a
jiji4000 <jiji4000alpha@gmail.com>
2018-02-19 12:43:48 +0900

    add ALDS_1_12_A:MinimumSpanningTree

---
+++ AizuOnlineJudge/ALDS_1_12_A:MinimumSpanningTree.cpp
@@ -0,0 +1,9 @@
+//
+//  ALDS_1_12_A:MinimumSpanningTree.cpp
+//  AizuOnlineJudge
+//
+//  Created by jiji on 2018/02/19.
+//  Copyright © 2018年 jiji4000. All rights reserved.
+//
+
+#include <stdio.h>
