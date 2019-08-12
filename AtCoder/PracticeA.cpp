caf438a87c297654d9ec354add8b54cde2918b0c
jiji4000 <jiji4000alpha@gmail.com>
2019-08-12 23:43:40 +0900

    add practiceA

---
+++ AtCoder/PracticeA.cpp
@@ -0,0 +1,24 @@
+//
+//  PracticeA.cpp
+//  AizuOnlineJudge
+//
+//  Created by jiji on 2019/08/12.
+//  Copyright © 2019 jiji4000. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    int b,c;
+    cin >> b >> c;
+    string s;
+    cin >> s;
+    
+    cout << a + b + c << " " << s << endl;
+    return 0;
+}
