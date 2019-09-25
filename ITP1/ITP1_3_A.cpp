94d0e685c5452ebc1d12bbf960ec296e51820501
jiji4000 <jiji4000alpha@gmail.com>
2019-09-25 12:58:25 +0900

    add IPT1_3_A

---
+++ ITP1/ITP1_3_A.cpp
@@ -0,0 +1,21 @@
+//
+//  ITP1_3_A.cpp
+//  AizuOnlineJudge
+//
+//  Created by jiji on 2019/09/25.
+//  Copyright © 2019 jiji4000. All rights reserved.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+int main(void){
+    for(int i  = 0;i < 1000;++i){
+        cout << "Hello World" << endl;
+    }
+    
+    return 0;
+}
