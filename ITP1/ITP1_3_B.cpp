776a1e77fd683e23a4f5895b0f304267f28e002b
jiji4000 <jiji4000alpha@gmail.com>
2019-09-26 20:22:56 +0900

    add ITP1_3_B

---
+++ ITP1/ITP1_3_B.cpp
@@ -0,0 +1,30 @@
+//
+//  ITP1_3_B.cpp
+//  AizuOnlineJudge
+//
+//  Created by jiji on 2019/09/25.
+//  Copyright © 2019 jiji4000. All rights reserved.
+//
+//#include <iostream>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//using namespace std;
+//int input[10000];
+//
+//int main(void){
+//    int index = 0;
+//    while(1){
+//        cin >> input[index];
+//        if(input[index] == 0){
+//            break;
+//        }
+//        ++index;
+//    }
+//    
+//    for(int i = 0;i < index;++i){
+//        cout << "Case " << i + 1 << ": " << input[i] << endl;
+//    }
+//    
+//    return 0;
+//}
