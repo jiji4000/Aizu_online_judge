a43cdd0a367c9248d9760c891e26d02d6216fed3
jiji4000 <jiji4000alpha@gmail.com>
2019-11-20 03:22:29 +0900

    add itp2_9_d

---
+++ ITP2/ITP2_9_D.cpp
@@ -0,0 +1,36 @@
+//
+//  ITP2_9_D.cpp
+//  AizuOnlineJudge
+//
+//  Created by jiji on 2019/11/20.
+//  Copyright © 2019 jiji4000. All rights reserved.
+//
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n1,num;
+    set<int> set1;
+    cin >> n1;
+    
+    for(int i = 0;i < n1;++i){
+        cin >> num;
+        set1.insert(num);
+    }
+    
+    cin >> n1;
+    for(int i = 0;i < n1;++i){
+        cin >> num;
+        if(!set1.insert(num).second){
+            set1.erase(num);
+        }
+    }
+    
+    for(set<int>::iterator it = set1.begin();it != set1.end();++it){
+        cout << *it << endl;
+    }
+}
