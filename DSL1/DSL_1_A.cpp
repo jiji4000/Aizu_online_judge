e7792a26cb8954adf4fbf525973f73dd9a13e73b
jiji4000 <jiji4000alpha@gmail.com>
2019-11-25 10:04:37 +0900

    remove all cache

--- DSL1/DSL_1_A.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  DSL_1_A.cpp
-//  AizuOnlineJudge
-//
-//  Created by jiji on 2019/11/25.
-//  Copyright © 2019 jiji4000. All rights reserved.
-//
-
-#include <stdio.h>
--- ITP1/ITP1_10_B.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ITP1_10_B.cpp
-//  AizuOnlineJudge
-//
-//  Created by jiji on 2019/10/14.
-//  Copyright © 2019 jiji4000. All rights reserved.
-//
-
-#include <stdio.h>
